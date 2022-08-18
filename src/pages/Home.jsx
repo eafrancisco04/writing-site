d54d4480a5406769eecc15884c21cd81e696b099
Ea Francisco <emberkasai04@gmail.com>
2022-08-17 20:28:44 -0400

    create initial cover

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,33 @@
-import cover1 from '../img/cover1';
+import cover1 from '../img/cover1.jpg';
+import styled from 'styled-components';
 
 function Home() {
     return(
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eaque corporis consequuntur cumque. Provident possimus nesciunt repudiandae recusandae debitis odio!</p>
-            <img src={cover1} />
-        </div>
-
+        <>
+        <Cover>
+            <img src={cover1} height={800}/>
+            <div className='latest-cover'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae repudiandae odio totam itaque ducimus distinctio esse eum consequatur! Voluptates assumenda magni atque qui placeat dolores!</p>
+            </div>
+        </Cover>
+        </>
     )
 }
 
+const Cover = styled.div`
+    margin: 2em;
+    padding: 3em;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+
+    h3 {
+        color: #FEC437;
+    }
+
+    .latest-cover {
+        padding: 3em;
+    }
+`
 
 export default Home;
